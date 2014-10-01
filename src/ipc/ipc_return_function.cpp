2eeccffa341a50fdd302d2d69f3559fe707e61d9
Terence-Lee Davis <terencelee.davis@edelman.com>
2014-10-01 11:13:30 +0100

    Added return function parsing

--- src/ipc/ipc_return_function.cpp
+++ src/ipc/ipc_return_function.cpp
@@ -1,11 +1,94 @@
 #include "ipc_return_function.h"
 
-IPCReturnFunction::IPCReturnFunction() {
+#include "../globals.h"
+
+#include <netinet/in.h>
+#include <cstring>
+
+IPCReturnFunction::IPCReturnFunction()
+{
 }
 
-IPCReturnFunction::IPCReturnFunction(const IPCReturnFunction& orig) {
+IPCReturnFunction::IPCReturnFunction(const IPCReturnFunction& orig) {}
+IPCReturnFunction::~IPCReturnFunction()
+{
+	// Free up arg memory
+	unsigned int args = this->argv.size();
+	for(unsigned int i = 0; i < args; i++)
+	{
+		if(this->argt[i] == IPCTypes::uint || this->argt[i] == IPCTypes::sint)
+			delete (uint32_t*) this->argv[i];
+		else if(this->argt[i] == IPCTypes::ch)
+			delete [] (char*) this->argv[i];
+	}
+	
+	delete [] this->functionName;
+	
+	// Decide how to delete the return based on its stored type
+	if(this->functionReturnType == IPCTypes::uint || this->functionReturnType == IPCTypes::sint)
+		delete (uint32_t*) this->functionReturnPtr;
+	else if(this->functionReturnType == IPCTypes::ch)
+		delete [] (char*) this->functionReturnPtr;
 }
 
-IPCReturnFunction::~IPCReturnFunction() {
+void IPCReturnFunction::Parse(char* payload, bool destructive)
+{
+	unsigned int cursor = 0;
+	
+	// - Packet ID
+	this->packetID = ntohl(*(uint32_t*) payload);
+	cursor += 4;
+	
+	// - Function name size
+	unsigned int funcSize = ntohl(*(uint32_t*) payload + cursor);
+	cursor += 4;
+	
+	// - Function name
+	this->functionName = new char[funcSize + 1];
+	this->functionName[funcSize] = '\0';
+	memcpy(this->functionName, payload + cursor, funcSize);
+	cursor += funcSize;
+	
+	// - Arg count
+	uint8_t argc = *(uint8_t*) (payload + cursor);
+	cursor += 1;
+	
+	// - Args
+	this->argv.reserve(argc);
+	this->argt.reserve(argc);
+	for(unsigned int i = 0; i < argc; i++)
+	{
+		// -- Type
+		this->argt.push_back(*(uint8_t*) (payload + cursor));
+		cursor += 1;
+		
+		// -- Value
+		if(this->argt[i] == IPCTypes::uint || this->argt[i] == IPCTypes::sint)
+		{
+			// We first have to convert the network byte order integer to host
+			// order then make space for it in memory so we can keep it for
+			// later use.
+			uint32_t integer = ntohl(*(uint32_t*) (payload + cursor));
+			char* intSpace = new char[4];
+			
+			memcpy(intSpace, &integer, 4);
+			this->argv.push_back(intSpace);
+			
+			cursor += 4;
+		}
+		else if(this->argt[i] == IPCTypes::ch)
+		{
+			// --- Size of string
+			uint32_t s = ntohl(*(uint32_t*) (payload + cursor));
+			cursor += 4;
+			
+			// --- String value
+			char* str = new char[s + 1];
+			memcpy(str, payload + cursor, s);
+			str[s] = '\0';
+			this->argv.push_back(str);
+			
+			cursor += s;
+		}
+	}
 }
-
--- src/ipc/ipc_return_function.h
+++ src/ipc/ipc_return_function.h
@@ -2,6 +2,7 @@
 #define	IPC_RETURN_FUNCTION_H
 
 #include <vector>
+#include <stdint.h>
 
 class IPCReturnFunction {
 public:
@@ -15,7 +16,7 @@ public:
      * @param packet
 	 * @param destructive If true, will consume and delete the packet.
      */
-	void Parse(char* packet, bool destructive);
+	void Parse(char* payload, bool destructive);
 	
 	/**
 	 * Executes the function and puts the return into public variables.
@@ -23,11 +24,13 @@ public:
 	void Execute();
 	
 	void* functionReturnPtr;
-	unsigned int functionReturnType;
+	uint32_t functionReturnType;
 private:
 	char* functionName;
 	std::vector<void*> argv;
 	std::vector<unsigned int> argt;
+	
+	uint32_t packetID;
 };
 
 #endif	/* IPC_RETURN_FUNCTION_H */
